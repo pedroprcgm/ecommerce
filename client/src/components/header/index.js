ddb3615b7347cd1199c834e09b38f53dbc52a5f2
Pedro Guerreiro <pedro.prcgm@gmail.com>
2018-07-17 12:14:05 +0100

    Set text title by search input

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
-import { PageHeader } from 'react-bootstrap';
-
+import PubSub from 'pubsub-js';
 class Header extends Component {
+
+    constructor() {
+        super();
+        this.defaultTitle = 'Todos os produtos';
+        this.state = { title: this.defaultTitle };
+        this.setTitle = this.setTitle.bind(this);
+    }
+
+    componentDidMount() {
+        PubSub.subscribe('search', this.setTitle);
+    }
+
+    setTitle(name, data) {
+        this.setState({ title: data.value || this.defaultTitle });
+    }
 
     render() {
         return (
             <div className="header-wrapper">
                 <div className="container">
                     <div className="header">
-                        <h1 className="title">{this.props.title}</h1>
+                        <h1 className="title">{this.state.title}</h1>
                     </div>
                 </div>
             </div>
