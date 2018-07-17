00ea91ad2dcd72d505812e61b7690ec3f34af2cd
Pedro Guerreiro <pedro.prcgm@gmail.com>
2018-07-17 23:45:21 +0100

    Improve style

--- client/src/components/product-item/index.js
+++ client/src/components/product-item/index.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
-import { ListGroupItem } from 'react-bootstrap';
 
 class ProductItem extends Component {
 
     render() {
         return (
-            <ListGroupItem className="product-item">
-                <div className="pull-right">
-                    <span className="old-price">R${this.props.item.price}</span>
-                    &nbsp;por&nbsp;
-                    <span className="promo-price">R${this.props.item.promoPrice}</span>
+            <li className="product-item row">
+                <div className="col-xs-6 col-sm-9">
+                    <div className="image-wrapper">
+                        <img src="./img/1.jpg" alt="Imagem do produto" />
+                        <img src="./img/2.jpg" alt="Imagem do produto" />
+                        <img src="./img/3.jpg" alt="Imagem do produto" />
+                        <img src="./img/4.jpg" alt="Imagem do produto" />
+                    </div>
+                    <div className="product-info">
+                        <p className="item-name">{this.props.item.name}</p>
+                        <span className="item-description">{this.props.item.description}</span>
+                    </div>
                 </div>
-            </ListGroupItem>
+                <div className="col-xs-6 col-sm-3">
+                    <div className="price-wrapper">
+                        <span className="old-price">R${this.props.item.price}</span>
+                        &nbsp;por&nbsp;
+                        <span className="promo-price">R${this.props.item.promoPrice}</span>
+                    </div>
+                </div>
+            </li>
         )
     }
 }
